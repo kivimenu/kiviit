5e3d9debebce86e1dca89be4a5086996ba8e730b
deeppaz <deeppaz@hotmail.com>
2020-04-26 21:04:20 +0300

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,49 +3,79 @@ import React, { Component } from 'react'
 class About extends Component {
     render() {
         return (
-            <section className="about-section pd-t-120" id="about">
+            <section className="section section-about" id="about">
                 <div className="container">
-                    <div className="row justify-content-center ml-b-30">
-                        <div className="col-lg-5 mrb-30">
-                            <div className="about-thumb-area">
-                                <div className="about-thumb">
-                                    <img src={require('../img/downbanner.png')} alt="about" />
-                                </div>
-                            </div>
+                    <div className="section-head">
+                        <span>Get to know</span>
+                        <h2>About Me</h2>
+                    </div>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col-md-5">
+                            <img className="img-fluid img-thumbnail" src="assets/images/avatar.jpg" alt="About Picture" />
                         </div>
-                        <div className="col-lg-7 mrb-30">
-                            <div className="about-content-area">
-                                <span className="sub-title">About The Desease</span>
-                                <h3 className="title">Basically We Should Follow The Advice From Healhcare</h3>
-                                <div className="about-item-area ml-b-60">
-                                    <div className="about-item d-flex flex-wrap align-items-center mrb-60">
-                                        <div className="about-icon">
-                                            <i className="fas fa-home"></i>
+                        <div className="col-md-7">
+                            <div className="about-desc">
+                                <div className="about-desc-content">
+                                    <h1 className="font-weight-light">I'm Alice Joseph</h1>
+                                    <p className="mt-3 font-weight-light">Proin laoreet elementum ligula, ac tincidunt lorem accumsan nec. Fusce eget urna ante. Donec massa velit, varius a accumsan ac, tempor iaculis massa. Sed placerat justo sed libero varius vulputate. Ut a mi tempus massa malesuada fermentum.</p>
+                                </div>
+
+                                <div className="about-desc-more text-left mt-5">
+                                    <div className="row">
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="far fa-calendar-alt" data-toggle="tooltip" data-placement="top" title="Age"></i>
+                                                <b>Age : </b>
+                                                <span>19</span>
+                                            </div>
                                         </div>
-                                        <div className="about-item-content">
-                                            <h3 className="item-title">Stay At Home</h3>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam excepturi quasi voluptatum eaque dolores?</p>
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="fas fa-map-marked-alt" data-toggle="tooltip" data-placement="top" title="Location"></i>
+                                                <b>Location : </b>
+                                                <span>Miami, FL</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="about-item d-flex flex-wrap align-items-center mrb-60">
-                                        <div className="about-icon">
-                                            <i className="fas fa-ambulance"></i>
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="fas fa-graduation-cap" data-toggle="tooltip" data-placement="top" title="Degree"></i>
+                                                <b>Degree : </b>
+                                                <span>Master</span>
+                                            </div>
                                         </div>
-                                        <div className="about-item-content">
-                                            <h3 className="item-title">Be Supportive</h3>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam excepturi quasi voluptatum eaque dolores?</p>
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="fab fa-skype" data-toggle="tooltip" data-placement="top" title="Skype"></i>
+                                                <b>Skype : </b>
+                                                <a href="tel:beingeorge">beingeorge</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="about-item d-flex flex-wrap align-items-center mrb-60">
-                                        <div className="about-icon">
-                                            <i className="fas fa-unlock-alt"></i>
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="fas fa-phone-alt" data-toggle="tooltip" data-placement="top" title="Phone"></i>
+                                                <b>Phone : </b>
+                                                <a href="tel:0987654321">098-765-4321</a>
+                                            </div>
                                         </div>
-                                        <div className="about-item-content">
-                                            <h3 className="item-title">Protect Yourself</h3>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam excepturi quasi voluptatum eaque dolores?</p>
+                                        <div className="col-lg-6">
+                                            <div className="about-desc-info">
+                                                <i className="far fa-envelope" data-toggle="tooltip" data-placement="top" title="Mail"></i>
+                                                <b>Mail : </b>
+                                                <a href="mailto:hello@beingeorge.com">hello@beingeorge.com</a>
+                                            </div>
                                         </div>
+
                                     </div>
                                 </div>
+
+                                <ul className="about-social mb-0 list-inline mt-4">
+                                    <li className="list-inline-item">
+                                        <a href="#" className="btn downloadCV hover-state">
+                                            <i className="fas fa-download"></i>
+                                            <span>Download CV</span>
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
---
+++ src/pages/Services.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+
+class Services extends Component {
+    render() {
+        return (
+            <section className="section section-dark section-services" id="services">
+                <div className="container">
+                    <div className="section-head">
+                        <span>WHAT I DO</span>
+                        <h2>My Services</h2>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="fas fa-paint-brush  "></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Graphic Design</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="fas fa-palette  "></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Web Development</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="fas fa-compact-disc  "></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Media Marketing</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="fa fa-puzzle-piece  " aria-hidden="true"></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Advetising</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="fa fa-gamepad  " aria-hidden="true"></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Game Development</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-lg-6">
+                            <div className="services-list hover-state p-4">
+                                <div className="services-list-icon b-box">
+                                    <i className="far fa-chart-bar  "></i>
+                                </div>
+                                <div className="mt-4">
+                                    <h5 className="mb-0">Analytics</h5>
+                                    <p className=" mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Services;
