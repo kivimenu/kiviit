dd86f5de329a05b51dd9180754dae0226a831a68
deeppaz <deeppaz@hotmail.com>
2020-05-01 23:29:23 +0300

    update

--- src/components/KFooter.js
+++ src/components/KFooter.js
@@ -5,7 +5,7 @@ class KFooter extends Component {
         return (
             <footer className="footer section section-light">
                 <div className="container">
-                    <p className="copyright text-center mb-0">© Kivi 2020 | All Right Reserved</p>
+                    <p className="copyright text-center mb-0">© Kivi IT 2020 | All Right Reserved</p>
                 </div>
             </footer>
         )
